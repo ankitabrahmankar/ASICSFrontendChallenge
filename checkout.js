2d831c8f1d9628b8e00e53c04358841157bab30e
ankitabrahmankar <brahmankar.a@northeastern.edu>
2021-04-22 15:13:24 -0400

    Adding JSON output to console

--- checkout.js
+++ checkout.js
@@ -1,21 +1,55 @@
-var stateByCountry = {
-    India: ["Maharashtra","Goa","Kerala"],
-    UnitedStates: ["MA","CA","CO"],
-    Australia: ["South Australia"," NorthAustralia"]
+// var stateByCountry = {
+//     India: ["Maharashtra","Goa","Kerala"],
+//     UnitedStates: ["MA","CA","CO"],
+//     Australia: ["South Australia"," NorthAustralia"]
+//     }
+//     function makeSubmenu(value) {
+//     if(value.length==0) 
+//     {
+//         document.getElementById("stateSelect").innerHTML = "<option></option>";
+//     }
+//     else {
+//     var stateOptions = ""; 
+//     console.log(stateByCountry[value]);
+//     for(stateId in stateByCountry[value]) {
+//         console.log(stateId);
+//         stateOptions+="<option>"+stateByCountry[value][stateId]+"</option>";
+//     }
+//     document.getElementById("stateSelect").innerHTML = stateOptions;
+//     }
+//     }
+
+const saveJSONData =() => {
+    var firstname, lastname, address1, address2, city, postalcode,phone,email, checkbox;
+    firstname = document.getElementById('FirstName').value;
+    lastname= document.getElementById('LastName').value;
+    address1= document.getElementById('Address1').value;
+    address2= document.getElementById('Address2').value;
+    city= document.getElementById('city').value;
+    postalcode= document.getElementById('postalcode').value;
+    phone= document.getElementById('phone').value;
+    email= document.getElementById('email').value;
+    checkbox= document.getElementById('marketing').value;
+
+    const output = {
+        "firstName": firstname,
+        "lastName": lastname,
+        "address": {
+            "address1": address1 ,
+            "address2": address2,
+            "city": city,
+            "state": "",
+            "country": "",
+            "postalCode": postalcode
+        },
+        "phone": phone,
+        "email": email,
+        "isSubscribed": checkbox
     }
-    function makeSubmenu(value) {
-    if(value.length==0) 
-    {
-        document.getElementById("stateSelect").innerHTML = "<option></option>";
-    }
-    else {
-    var stateOptions = ""; 
-    console.log(stateByCountry[value]);
-    for(stateId in stateByCountry[value]) {
-        console.log(stateId);
-        stateOptions+="<option>"+stateByCountry[value][stateId]+"</option>";
-    }
-    document.getElementById("stateSelect").innerHTML = stateOptions;
-    }
-    }
+    
+
+    console.log(output);
+}
+
+
   
